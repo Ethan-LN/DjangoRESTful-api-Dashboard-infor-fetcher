dffd973c9fd6eb88034959a60f4ef10013963f3c
Ethan <et.l.nicholas@gmail.com>
2023-08-05 00:49:31 +1000

    Jenkins update

--- envsetup.sh
+++ envsetup.sh
@@ -11,6 +11,8 @@ echo $PWD
 . env/bin/activate
 echo $PWD
 
+sudo apt-get install python3-dev default-libmysqlclient-dev build-essential pkg-config
+
 pip install -r requirements.txt
 
 if [ -d "logs" ]; then
